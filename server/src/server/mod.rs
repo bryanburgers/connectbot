e70add0fbc1e05c8208a56c5fbacb77cfa67f160
Bryan Burgers <bryan@burgers.io>
2018-10-07 21:14:22 -0500

    Move client_connection into its own file

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -20,6 +20,10 @@ use uuid::Uuid;
 
 use super::world::{self, SharedWorld};
 
+mod client_connection;
+
+use self::client_connection::ClientConnection;
+
 pub struct Server {
     world: SharedWorld,
     state: Arc<RwLock<HashMap<String, Connection>>>,
@@ -201,144 +205,5 @@ impl Server {
             // message_handler::handle_message(message, tx.clone(), new_state.clone())
             Box::new(futures::future::ok(()))
         })
-    }
-}
-
-struct ClientConnection {
-    id: String,
-    client_id: Option<String>,
-    world: SharedWorld,
-}
-
-impl ClientConnection {
-    fn new(id: String, world: SharedWorld) -> ClientConnection {
-        ClientConnection {
-            id: id,
-            client_id: None,
-            world: world,
-        }
-    }
-
-    pub fn handle_connection<S, C>(&mut self, addr: SocketAddr, conn: TlsStream<S, C>) -> impl Future<Item=(), Error=std::io::Error>
-        where S: tokio::io::AsyncWrite + tokio::io::AsyncRead + Send + 'static,
-              C: rustls::Session + 'static,
-    {
-        // Process socket here.
-        let codec: Codec<client::ServerMessage, client::ClientMessage> = Codec::new();
-        let framed = tokio_codec::Decoder::framed(codec, conn);
-
-        let (sink, stream) = framed.split();
-
-        let stream = TimedConnection::new(stream, TimedConnectionOptions { ..Default::default() });
-
-        let (tx, rx) = futures::sync::mpsc::channel(0);
-
-        let sink = sink.sink_map_err(|_| ());
-        let rx = rx.map_err(|_| panic!());
-
-        let client_id = self.id.clone();
-        tokio::spawn(rx.map(move |message| { println!("↓ {}: {:?}", client_id, message); message }).forward(sink).then(|result| {
-            if let Err(e) = result {
-                panic!("failed to write to socket: {:?}", e)
-            }
-            Ok(())
-        }));
-
-        let id = self.id.clone();
-        let world_disconnect = self.world.clone();
-        let id_disconnect = self.id.clone();
-
-        stream.for_each(move |message| -> Box<dyn Future<Item=(), Error=std::io::Error> + Send> {
-            match message {
-                TimedConnectionItem::Item(message) => {
-                    println!("↑ {}: {:?}", id.clone(), message);
-                    /*
-                    {
-                        let id = id.clone();
-                        let mut hash_map = state.write().unwrap();
-                        hash_map.entry(id)
-                            .and_modify(|client| {
-                                client.last_message = Some(Instant::now());
-                            });
-                    }
-                    */
-
-                    if message.has_ping() {
-                        let pong = client::Pong::new();
-                        let mut message = client::ServerMessage::new();
-                        message.set_pong(pong);
-
-                        let f = tx.clone().send(message)
-                            .map(|_| ())
-                            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e)));
-
-                        return Box::new(f);
-                    }
-
-                    if message.has_initialize() {
-                        println!("Initialized!");
-
-                        // TODO: Do stuff. Maybe disconnect any existing items. Store some data.
-                        // Update some thingses.
-                    }
-                    /*
-                    if message.has_clients_request() {
-                        let clients_response = control::ClientsResponse::new();
-                        let mut response = control::ServerMessage::new();
-                        response.set_clients_response(clients_response);
-                        response.set_in_response_to(message.get_message_id());
-
-                        /*
-                        return tx.clone().send(response)
-                            .map(|_| ())
-                            .map_err(|_| ());
-                            */
-
-                        let f = tx.clone().send(response)
-                            .map(|_| ())
-                            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e)));
-                            // .map_err(|_| ());
-
-                        return Box::new(f);
-
-                            /*
-                        let f = tx.clone().send(response)
-                            .map(|_| ())
-                            .map_err(|_| ());
-                        tokio::spawn(f);
-                        */
-                    }
-                */
-
-                    // message_handler::handle_message(message, tx.clone(), new_state.clone())
-                    Box::new(futures::future::ok(()))
-                },
-                TimedConnectionItem::Timeout => {
-                    let ping = client::Ping::new();
-                    let mut response = client::ServerMessage::new();
-                    response.set_ping(ping);
-
-                    let f = tx.clone().send(response)
-                        .map(|_| ())
-                        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to send ping: {}", e)));
-
-                    Box::new(f)
-                },
-            }
-        })
-            .and_then(move |_| {
-                let uuid = id_disconnect.clone();
-                let world = world_disconnect.clone();
-                {
-                    let mut world = world.write().unwrap();
-                    world.devices.entry(uuid.clone())
-                        .and_modify(|device| {
-                            device.connection_status = world::ConnectionStatus::Disconnected { last_seen: Instant::now() };
-                        });
-                }
-                println!("Disconnect? {:?}", uuid);
-
-                Ok(())
-            })
     }
 }
